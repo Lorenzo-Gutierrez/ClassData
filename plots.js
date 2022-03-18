b287dfbab17b3c69c7a504ef7fea1f0c3b6b6642
Lorenzo Miguel Gutierrez <el2tha.g@gmail.com>
2022-03-17 21:05:21 -0700

    updated all files

--- plots.js
+++ plots.js
@@ -1,46 +1,37 @@
-// Submit Button handler
-function handleSubmit() {
-  // Select the input value from the form
-  var city = d3.select("#cityInput").node().value;
+// Sort the data array using the greekSearchResults value
+data.sort(function(a, b) {
+  return parseFloat(b.greekSearchResults) - parseFloat(a.greekSearchResults);
+});
 
-  // clear the input value
-  d3.select("#cityInput").node().value = "";
+// Slice the first 10 objects for plotting
+data = data.slice(0, 10);
 
-  // Build the plot with the new city
-  buildPlot(city);
-}
+// Reverse the array due to Plotly's defaults
+data = data.reverse();
 
-function buildPlot(city) {
-  var apiKey = "YOUR KEY HERE";
+// Trace1 for the Greek Data
+var trace1 = {
+  x: data.map(row => row.greekSearchResults),
+  y: data.map(row => row.greekName),
+  text: data.map(row => row.greekName),
+  name: "Greek",
+  type: "bar",
+  orientation: "h"
+};
 
-  let url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=imperial`
+// data
+var data = [trace1];
 
-  d3.json(url).then(function(data) {
-    console.log(data);
+// Apply the group bar mode to the layout
+var layout = {
+  title: "Greek gods search results",
+  margin: {
+    l: 100,
+    r: 100,
+    t: 100,
+    b: 100
+  }
+};
 
-    var times = data.list.map(x => x.dt_txt);
-    var temps = data.list.map(x => x.main.temp);
-
-    var trace1 = {
-      type: "scatter",
-      mode: "lines",
-      x: times,
-      y: temps,
-      line: {
-        color: "#17BECF",
-      }
-    };
-
-    var plotData = [trace1];
-
-    var layout = {
-      title: `${data.city.name} 5-day forecast`,
-    };
-
-    Plotly.newPlot("plot", plotData, layout);
-  })
-
-}
-
-// Add event listener for submit button
-d3.select("#submit").on("click", handleSubmit);
+// Render the plot to the div tag with id "plot"
+Plotly.newPlot("plot", data, layout);
